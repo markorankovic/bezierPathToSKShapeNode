3b982783416cb4674895daf866cbf8fed271f323
Marko <markocrush96@gmail.com>
2018-03-18 23:13:35 +0000

    Make an extension for SkShapeNode

---
+++ SKShapeNodeExt.playground/Contents.swift
@@ -0,0 +1,20 @@
+
+
+
+import PlaygroundSupport
+
+import SpriteKit
+
+
+
+let view = SKView(frame: .init(x: 0, y: 0, width: 500, height: 500))
+let scene = SKScene(size: .init(width: view.bounds.width, height: view.bounds.height))
+scene.backgroundColor = .orange
+view.presentScene(scene)
+PlaygroundPage.current.liveView = view
+
+extension SKShapeNode {
+    
+    
+    
+}
