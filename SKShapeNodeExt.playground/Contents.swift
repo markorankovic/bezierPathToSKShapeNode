7c68281202785daf30529acc295e0d5be24cc8e8
Marko <markocrush96@gmail.com>
2018-03-19 00:14:37 +0000

    More balls!

--- SKShapeNodeExt.playground/Contents.swift
+++ SKShapeNodeExt.playground/Contents.swift
@@ -19,7 +19,7 @@ extension SKShapeNode {
     
 }
 
-let points = stride(from: 0, to: 2 * Double.pi, by: 0.1).map{ CGPoint(x: $0 * 50, y: sin($0) * 50) }
+let points = stride(from: 0, to: 6 * Double.pi, by: 0.1).map{ CGPoint(x: $0 * 70, y: sin($0) * 50) }
 
 let path = UIBezierPath()
 
@@ -32,5 +32,17 @@ for (i, p) in points.enumerated() {
 }
 
 let sineWave = SKShapeNode(path: path)
-sineWave.position.x -= 100
+sineWave.position.x -= 200
+sineWave.physicsBody = SKPhysicsBody(edgeChainFrom: path.cgPath)
+sineWave.physicsBody?.isDynamic = false
 scene.addChild(sineWave)
+
+for _ in 0..<100 {
+    let ball = SKShapeNode(circleOfRadius: 20)
+    ball.physicsBody = SKPhysicsBody(circleOfRadius: 20)
+    ball.position = CGPoint(x: 0, y: 300)
+    ball.physicsBody?.isDynamic = true
+    scene.addChild(ball)
+}
+
+
